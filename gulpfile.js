8435c1cc65fcc8100a7f13b2d67a29c8f71193b2
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2016-09-27 12:15:21 +0100

    Basic CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 /*
-* ES6 Boilerplate for project Automation
-* Inspired by Jean-Pierre Sierens.
-* Author: Jean-Philippe Drecourt
-*/
+ * ES6 Boilerplate for project Automation
+ * Inspired by Jean-Pierre Sierens.
+ * Author: Jean-Philippe Drecourt
+ */
 
 // Declarations & dependencies
 // ----------------------------------------------------------------------------
@@ -12,7 +12,10 @@ var browserify = require('browserify'); // Turning nodeJS into browser compatibl
 var source = require('vinyl-source-stream'); // Allows to use normal text streams
 var babelify = require('babelify'); // Transpiler
 var webserver = require('gulp-webserver'); // Webserver with LiveReaload
+var sass = require('gulp-sass'); // SASS compiler
+var prefix = require('gulp-autoprefixer'); // Browser compatibility
 var htmlPages = ['src/*.html']; // HTML pages to watch
+var scssPages = ['src/*.scss']; // CSS pages to watch
 
 // External dependencies that don't need to be rebundled while developing
 // They'll be bundled once and for all in 'vendor.js'
@@ -26,40 +29,61 @@ var scriptsCount = 0;
 //Gulp tasks
 // ----------------------------------------------------------------------------
 // Development task - Don't bundle the dependencies
-gulp.task('scripts', function () {
+gulp.task('scripts', function() {
   bundleApp(false);
 });
+// SASS, CSS and prefix
+gulp.task('sass', function() {
+  return gulp.src(scssPages)
+    .pipe(sass({
+      indentedSyntax: true
+    }).on('error', sass.logError))
+    .pipe(prefix({
+      browsers: ['last 15 versions', '> 1%', 'ie 8', 'ie 7'],
+      cascade: true
+    }))
+    .pipe(gulp.dest('dev/web/css'));
+});
 // Copy HTML page in separate task to avoid recompiling JS
-// TODO CSS pages
-gulp.task('copy-html', function () {
+gulp.task('copy-html', function() {
   gulp.src(htmlPages)
     .pipe(gulp.dest('dev'));
 });
 // Webserver task for Development
-gulp.task('webserver', function () {
+gulp.task('webserver', function() {
   gulp.src('./dev/')
     .pipe(webserver({
-        livereload: true,
-        open: true
+      livereload: true,
+      open: true
     }));
 });
 // Deployment task - Bundle everything into one script and copy HTML pages
 // Destination directory: ./dist
-gulp.task('deploy', function () {
+gulp.task('deploy', function() {
   gulp.src(htmlPages)
     .pipe(gulp.dest('dist'));
+  gulp.src(scssPages)
+    .pipe(sass({
+      indentedSyntax: true
+    }).on('error', sass.logError))
+    .pipe(prefix({
+      browsers: ['last 15 versions', '> 1%', 'ie 8', 'ie 7'],
+      cascade: true
+    }))
+    .pipe(gulp.dest('dist/web/css'));
   bundleApp(true);
 });
 
 // Watch task - Reruns scripts task everytime something changes
 // Destination directory: dist
-gulp.task('watch', function () {
+gulp.task('watch', function() {
   gulp.watch(['./src/*.js'], ['scripts']);
   gulp.watch(['./src/*.html'], ['copy-html']);
+  gulp.watch(['./src/*.scss'], ['sass']);
 });
 
 // Default task for development - Called by 'gulp' on terminal
-gulp.task('default', ['scripts', 'copy-html', 'webserver', 'watch']);
+gulp.task('default', ['scripts', 'copy-html', 'sass', 'webserver', 'watch']);
 
 // Private Functions
 // ----------------------------------------------------------------------------
@@ -80,9 +104,9 @@ function bundleApp(isProduction) {
   // If the file exists, then do not recreate it.
   if (!isProduction && scriptsCount === 1) {
     browserify({
-      require: dependencies,
-      debug: true
-    })
+        require: dependencies,
+        debug: true
+      })
       .bundle()
       .on('error', gutil.log)
       .pipe(source('vendors.js'))
@@ -92,20 +116,20 @@ function bundleApp(isProduction) {
     rootDir = './dist';
     // Copy all the HTML files there to ease copying
 
-  }
-  else
-  {
+  } else {
     rootDir = './dev';
     // Make the dependencies external to avoid bundling in bundle.js
     // Dependencies are bundled in vendor.js in development
-    dependencies.forEach(function (dep) {
+    dependencies.forEach(function(dep) {
       appBundler.external(dep);
     });
   }
 
   appBundler
-    // Transform ES6 and JSX to ES5 with babelify
-    .transform('babelify', {presets: ['es2015']})
+  // Transform ES6 and JSX to ES5 with babelify
+    .transform('babelify', {
+      presets: ['es2015']
+    })
     .bundle()
     .on('error', gutil.log)
     .pipe(source('bundle.js'))
